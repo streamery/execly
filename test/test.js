a0c44ee6a9e22922a0a6fd41405829e7213a3f98
Joseph Werle <joseph.werle@gmail.com>
2014-04-06 13:35:32 -0400

    more clean up

--- test/test.js
+++ test/test.js
@@ -4,11 +4,9 @@
  */
 
 var exec = require('../')
-  , fs = require('fs')
 
 var HOME = process.env.HOME;
 var TERM = process.env.TERM;
-var read = fs.readFileSync;
 
 exec('ls', [HOME])
 .pipe(exec('localhost', __dirname+'/test.sh'))
