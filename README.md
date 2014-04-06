d249d5955948bf46ca79883a5a338364ffbbe578
Joseph Werle <joseph.werle@gmail.com>
2014-04-06 13:35:10 -0400

    clean up test

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ execly
 ======
 
 Streaming process executor, local and remote
+
+## install
+
+```sh
+$ npm install execly
+```
+
+## example
+
+```js
+var exec = require('execly');
+```
+
+## usage
+
+
+## license
+
+MIT
--- test/test.js
+++ test/test.js
@@ -10,8 +10,6 @@ var HOME = process.env.HOME;
 var TERM = process.env.TERM;
 var read = fs.readFileSync;
 
-exec('ls', [HOME]).pipe(
-  exec('localhost', __dirname+'/test.sh', [], {
-    privateKey: read(HOME +'/.ssh/id_rsa')
-}))
+exec('ls', [HOME])
+.pipe(exec('localhost', __dirname+'/test.sh'))
 .pipe(process.stdout);
